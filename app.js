de2081dd8577665f38944b30aa7d2ede4d9a4a06
Muqtadirat <muqtadir.my@gmail.com>
2024-01-20 16:13:51 +0100

    feat: canvas change button

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 "use strict";
 
-const grid = document.querySelectorAll(".grid");
+const container = document.querySelector(".container");
+const canvasBtn = document.querySelector(".grid-setter");
+// var grid;
+// var gridBox;
 
-grid.forEach((item) => {
-  item.addEventListener("mousemove", () => {
-    item.style.cssText = "background-color:black; opacity: 1";
-  });
-});
+function createGrids(canvasChange) {
+  container.innerHTML = "";
+  let x = canvasChange;
+
+  for (let gridBox = 0; gridBox < x; gridBox++) {
+    const gridBox = document.createElement("div");
+    gridBox.setAttribute("class", "gridbox");
+
+    container.appendChild(gridBox);
+
+    for (let grid = 0; grid < x; grid++) {
+      const grid = document.createElement("div");
+      grid.setAttribute("class", "grid");
+      gridBox.appendChild(grid);
+
+      grid.addEventListener("mousemove", () => {
+        grid.style.cssText = "background-color:black; opacity: 1";
+      });
+    }
+  }
+}
+
+const canvasChange = () => {
+  let canvasSize = +prompt("Pick your canvas");
+
+  createGrids(canvasSize);
+};
+
+createGrids(16);
+
+canvasBtn.addEventListener("click", canvasChange);
