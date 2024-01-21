955234754674ae93959c8b1452b4558200e698fa
Muqtadirat <muqtadir.my@gmail.com>
2024-01-21 15:53:16 +0100

    feat: clear and erase button

--- app.js
+++ app.js
@@ -2,23 +2,27 @@
 
 const container = document.querySelector(".container");
 const canvasBtn = document.querySelector(".grid-setter");
-// var grid;
-// var gridBox;
+const eraser = document.querySelector(".eraser");
+const clearBtn = document.querySelector(".clear");
+const rainbowBtn = document.querySelector(".rainbow-pen");
+// let gridBox;
+let grids = [];
 
 function createGrids(canvasChange) {
   container.innerHTML = "";
   let x = canvasChange;
 
-  for (let gridBox = 0; gridBox < x; gridBox++) {
+  for (let col = 0; col < x; col++) {
     const gridBox = document.createElement("div");
     gridBox.setAttribute("class", "gridbox");
 
     container.appendChild(gridBox);
 
-    for (let grid = 0; grid < x; grid++) {
+    for (let row = 0; row < x; row++) {
       const grid = document.createElement("div");
       grid.setAttribute("class", "grid");
       gridBox.appendChild(grid);
+      grids.push(grid);
 
       grid.addEventListener("mousemove", () => {
         grid.style.cssText = "background-color:black; opacity: 1";
@@ -35,4 +39,20 @@ const canvasChange = () => {
 
 createGrids(16);
 
+const clearCanvas = () => {
+  grids.forEach((grid) => {
+    grid.style.backgroundColor = "white";
+  });
+};
+
+const erase = () => {
+  grids.forEach((grid) => {
+    grid.addEventListener("mousemove", () => {
+      grid.style.cssText = "background-color: white";
+    });
+  });
+};
+
 canvasBtn.addEventListener("click", canvasChange);
+clearBtn.addEventListener("click", clearCanvas);
+eraser.addEventListener("click", erase);
