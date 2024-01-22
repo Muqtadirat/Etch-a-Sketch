90fedf958893bc2426794e92a2e2a186e4748686
Muqtadirat <muqtadir.my@gmail.com>
2024-01-22 08:30:58 +0100

    feat: rainbow button

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const canvasBtn = document.querySelector(".grid-setter");
 const eraser = document.querySelector(".eraser");
 const clearBtn = document.querySelector(".clear");
 const rainbowBtn = document.querySelector(".rainbow-pen");
+const colorBtn = document.querySelector(".pen-color");
 // let gridBox;
 let grids = [];
 
@@ -45,6 +46,14 @@ const clearCanvas = () => {
   });
 };
 
+const color = () => {
+  grids.forEach((grid) => {
+    grid.addEventListener("mousemove", () => {
+      grid.style.cssText = "background-color:black; opacity: 1";
+    });
+  });
+};
+
 const erase = () => {
   grids.forEach((grid) => {
     grid.addEventListener("mousemove", () => {
@@ -53,6 +62,41 @@ const erase = () => {
   });
 };
 
+// const rainbow = () => {
+//   let randomR = Math.floor(Math.random() * 256);
+//   let randomG = Math.floor(Math.random() * 256);
+//   let randomB = Math.floor(Math.random() * 256);
+
+//   grids.forEach((grid) => {
+//     grid.addEventListener("mousemove", () => {
+//       grid.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+//     });
+//   });
+// };
+
+const rainbow = () => {
+  grids.forEach((grid) => {
+    grid.addEventListener("mousemove", (event) => {
+      const x = event.clientX / window.innerWidth;
+      const y = event.clientY / window.innerHeight;
+
+      const hue = Math.atan2(y - 0.5, x - 0.5) + Math.PI * 2;
+      const saturation = 1;
+      const lightness = 0.5;
+
+      const color = `hsl(${(hue * 180) / Math.PI}, ${saturation * 100}%, ${
+        lightness * 100
+      }%)`;
+
+      grid.style.backgroundColor = color;
+    });
+  });
+};
+
+
+
 canvasBtn.addEventListener("click", canvasChange);
 clearBtn.addEventListener("click", clearCanvas);
+colorBtn.addEventListener("click", color);
 eraser.addEventListener("click", erase);
+rainbowBtn.addEventListener("click", rainbow);
